7f2a96dd11b93275738ec0546278fe2bd5fe1978
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-05-30 19:56:26 +0200

    use redux combineReducers nsted of man check state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,11 @@
+import { combineReducers } from 'redux'
+
 import counter from './counter';
 import todos from './todo';
 
-function reduce(state, action) { 
-    if (state === undefined) {
-        return {
-            counter: counter(undefined, action),
-            todos: todos(undefined, action)
-        }
-    } else {
-        return {
-            counter: counter(state.counter, action),
-            todos: todos(state.todos, action)
-        }
-    }
-}
+let reduce = combineReducers({
+    counter: counter,
+    todos: todos
+})
 
 export default reduce;

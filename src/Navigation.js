d9e6262fb201a105a55387c0e2fb8032b85d62b4
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-05-31 17:17:26 +0200

    reuse state in Navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { connect } from 'react-redux'
+
 import { Link } from 'react-router-dom'
 
 class Navigation extends React.Component {
@@ -8,13 +10,23 @@ class Navigation extends React.Component {
         return (
             <ul>
                 <li><Link to="/">HOME</Link></li>
-                <li><Link to="/todo">TODO</Link></li>
-                <li><Link to="/clicker">CLICKER</Link></li>
+                <li><Link to="/todo">TODO ({ this.props.todoCount })</Link></li>
+                <li><Link to="/clicker">CLICKER ({ this.props.counter })</Link></li>
             </ul>
 
         )
     }
-
 }
 
-export default Navigation;
+let mapStateToProps = (state) => {
+    return {
+        counter: state.counter,
+        todoCount: state.todos.length
+    }
+}
+
+let mapDispatchToProps = { }
+
+let ConnectedNav = connect(mapStateToProps, mapDispatchToProps)(Navigation);
+
+export default ConnectedNav;

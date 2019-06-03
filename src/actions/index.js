5335cf17357774cdc747670208099a8b9a44731b
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-06-03 20:25:49 +0200

    add action for Wiki search

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,30 @@
+
+import superagent from 'superagent';
+import jsonp from 'superagent-jsonp';
+
 export function incrementCounter() {
-    return {type: "INCREMENT"};
+    return { type: "INCREMENT" };
 }
 
 export function decrementCounter() {
-    return {type: "DECREMENT"};
+    return { type: "DECREMENT" };
 }
 
 export function addTodo(title) {
-    return {type: "TODO_ADD", title: title};
+    return { type: "TODO_ADD", title: title };
+}
+
+export function wikiSearch(keyword) {
+    return function(dispatch) {
+        let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${keyword}&format=json&callback=JSONP_CALLBACK`;
+        superagent
+            .get(url)
+            .use(jsonp({
+                callbackName: 'JSONP_CALLBACK',
+                timeout: 3000
+            }))
+            .end((error, response) => {
+                dispatch({ type: "WIKIPEDIA_SEARCH", keyword: keyword, response: response });
+            });
+    }
 }
